be16c1208aea694b43d4dcf10995aaff9259f3af
Sckmdg <archer1strong@gmail.com>
2017-03-02 17:48:37 +0600

    - Fix returns in reducer - still working on it

--- src/reducers/canvas.js
+++ src/reducers/canvas.js
@@ -5,19 +5,33 @@ import {
     initialState
 } from '../constants/constants';
 
+// export function canvas(state = initialState, action) {
+//     switch (action.type) {
+//         case CREATE_CANVAS:
+//             Object.assign(state, {klass: action.payload}) ;
+//             return state;
+//         case CREATE_TEXT:
+//             state.klass.add(action.payload);
+//             Object.assign(state, {emptySize: true});
+//             state.klass.renderAll();
+//             return state;
+//         case BOLD_TEXT:
+//             Object.assign(state, {checkBold: !state.checkBold});
+//             return state;
+//         default:
+//             return state
+//     }
+// }
+
 export function canvas(state = initialState, action) {
     switch (action.type) {
         case CREATE_CANVAS:
-            Object.assign(state, {klass: action.payload}) ;
-            return state;
+            return Object.assign({}, state, {klass: action.payload});
         case CREATE_TEXT:
             state.klass.add(action.payload);
-            Object.assign(state, {emptySize: true});
-            state.klass.renderAll();
-            return state;
+            return Object.assign({}, state, {emptySize: true}, state.klass.renderAll());
         case BOLD_TEXT:
-            Object.assign(state, {checkBold: !state.checkBold});
-            return state;
+            return Object.assign({}, state, {checkBold: !state.checkBold});
         default:
             return state
     }

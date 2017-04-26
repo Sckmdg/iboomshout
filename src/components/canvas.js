64dfdcca2a2fa12794b0e31caba6351630421ab9
aminov <rustam.aminov@sibers.com>
2017-04-26 15:05:40 +0600

    test git2

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -8,6 +8,7 @@ class Canvas extends Component {
      * width and height taken from element canvas.
      */
     componentDidMount() {
+        console.log('2 commit in redux');
         let canvas = new fabric.Canvas('canvas');
 
         canvas.setBackgroundImage(windowGlass, canvas.renderAll.bind(canvas), {
